30786051f40586d064e0f37a5b5cfe3071c6511b
cirno42 <58635649+cirno42@users.noreply.github.com>
2022-01-15 05:18:48 +0800

    little codestyle fix

--- src/main/java/messageprocessing/RequestMessage.java
+++ src/main/java/messageprocessing/RequestMessage.java
@@ -4,38 +4,49 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class RequestMessage extends Message {
+
+    private static final int COMMAND_INDEX = 1;
+    private static final int RESERVED_ZEROS_INDEX = 2;
+    private static final int ADDRESS_TYPE_INDEX = 3;
+    private static final int LENGTH_OF_DNS_NAME_INDEX = 4;
+
+    private static final int LENGTH_OF_MESSAGE_WITHOUT_ADDRESS = 4;
+    private static final int IPV4_LENGTH = 4;
+    private static final int IPV6_LENGTH = 16;
+    private static final int PORT_LENGTH = 2;
+
     public RequestMessage(ByteBuffer buffer) {
         super(new byte[buffer.limit()]);
         buffer.get(this.data);
-        if (!RequestMessage.isCorrect(this.data)) {
-            throw new IllegalArgumentException();
+        if (!isCorrect()) {
+            throw new IllegalArgumentException("Request message isn't correct");
         }
     }
 
-    private static boolean isCorrect(byte[] data) {
-        if (data.length < 5) {
+    private boolean isCorrect() {
+        if (data.length < LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + PORT_LENGTH + 1) {
             return false;
         }
 
-        if (data[2] != 0x00) {
+        if (data[RESERVED_ZEROS_INDEX] != 0x00) {
             return false;
         }
 
-        switch (data[3]) {
+        switch (data[ADDRESS_TYPE_INDEX]) {
             case IPv4:
-                if (data.length != 10) {
+                if (data.length != LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV4_LENGTH + PORT_LENGTH) {
                     return false;
                 }
                 break;
 
             case IPv6:
-                if (data.length != 22) {
+                if (data.length != LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV6_LENGTH + PORT_LENGTH) {
                     return false;
                 }
                 break;
 
             case DOMAIN_NAME:
-                if (data.length != 7 + data[4]) {
+                if (data.length != LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + 1 + data[LENGTH_OF_DNS_NAME_INDEX] + PORT_LENGTH) {
                     return false;
                 }
                 break;
@@ -45,25 +56,26 @@ public class RequestMessage extends Message {
     }
 
     public static boolean isCorrectSizeOfMessage(ByteBuffer data) {
-        if (data.position() < 5) {
+        if (data.position() < LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + 1) {
             return false;
         }
 
-        switch (data.get(3)) {
+
+        switch (data.get(ADDRESS_TYPE_INDEX)) {
             case IPv4:
-                if (data.position() != 10) {
+                if (data.position() != LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV4_LENGTH + PORT_LENGTH) {
                     return false;
                 }
                 break;
 
             case IPv6:
-                if (data.position() != 22) {
+                if (data.position() != LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV6_LENGTH + PORT_LENGTH) {
                     return false;
                 }
                 break;
 
             case DOMAIN_NAME:
-                if (data.position() != 7 + data.get(4)) {
+                if (data.position() != LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + 1 + data.get(LENGTH_OF_DNS_NAME_INDEX) + PORT_LENGTH) {
                     return false;
                 }
                 break;
@@ -73,40 +85,46 @@ public class RequestMessage extends Message {
     }
 
     public boolean isCommand(byte command) {
-        return command == data[1];
+        return command == data[COMMAND_INDEX];
     }
 
     public byte getAddressType() {
-        return data[3];
+        if (data.length < ADDRESS_TYPE_INDEX + 1) {
+            return 0;
+        }
+        return data[ADDRESS_TYPE_INDEX];
     }
 
     public byte[] getDestAddress() {
+        if (!isCorrect()) {
+            return null;
+        }
         switch (this.getAddressType()) {
             case IPv4:
-                return Arrays.copyOfRange(data, 4, 8);
+                return Arrays.copyOfRange(data, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV4_LENGTH);
 
             case DOMAIN_NAME:
-                int length = data[4];
-                return Arrays.copyOfRange(data, 5, 5 + length);
+                int length = data[LENGTH_OF_DNS_NAME_INDEX];
+                return Arrays.copyOfRange(data, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + 1, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + 1 + length);
 
             case IPv6:
-                return Arrays.copyOfRange(data, 4, 20);
+                return Arrays.copyOfRange(data, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV6_LENGTH);
         }
 
         return null;
     }
 
     public short getDestPort() {
-        switch (data[3]) {
+        switch (data[ADDRESS_TYPE_INDEX]) {
             case IPv4:
-                return ByteBuffer.wrap(data, 8, 2).getShort();
+                return ByteBuffer.wrap(data, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV4_LENGTH, 2).getShort();
 
             case DOMAIN_NAME:
-                int length = data[4];
-                return ByteBuffer.wrap(data, 5 + length, 2).getShort();
+                int length = data[LENGTH_OF_DNS_NAME_INDEX];
+                return ByteBuffer.wrap(data, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + 1 + length, 2).getShort();
 
             case IPv6:
-                return ByteBuffer.wrap(data, 20, 2).getShort();
+                return ByteBuffer.wrap(data, LENGTH_OF_MESSAGE_WITHOUT_ADDRESS + IPV6_LENGTH, 2).getShort();
         }
 
         return -1;
--- src/main/java/messageprocessing/ResponseMessage.java
+++ src/main/java/messageprocessing/ResponseMessage.java
@@ -4,26 +4,27 @@ import java.util.Arrays;
 
 public class ResponseMessage extends Message {
     private final RequestMessage request;
-
+    private static final int SOCKS_VERSION_INDEX = 0;
+    private static final int RESPONSE_CODE_INDEX = 1;
     public ResponseMessage(RequestMessage request) {
         super(Arrays.copyOf(request.getBytes(), request.getBytes().length));
         this.request = request;
     }
 
     public byte[] create(boolean isConnected) {
-        data[0] = SOCKS_5;
-        data[1] = SUCCEEDED;
+        data[SOCKS_VERSION_INDEX] = SOCKS_5;
+        data[RESPONSE_CODE_INDEX] = SUCCEEDED;
 
         if (!request.isCommand(CONNECT_TCP)) {
-            data[1] = COMMAND_NOT_SUPPORTED;
+            data[RESPONSE_CODE_INDEX] = COMMAND_NOT_SUPPORTED;
         }
 
         if (!isConnected) {
-            data[1] = HOST_NOT_AVAILABLE;
+            data[RESPONSE_CODE_INDEX] = HOST_NOT_AVAILABLE;
         }
 
         if (request.getAddressType() == IPv6) {
-            data[1] = ADDRESS_TYPE_NOT_SUPPORTED;
+            data[RESPONSE_CODE_INDEX] = ADDRESS_TYPE_NOT_SUPPORTED;
         }
 
         return data;
